1156ad65d38380ed47ddcaf1b553d3f033229749
Oehler.Sven <Oehler.Sven@DESKTOP-ITUU61G.2631.skool.net>
2018-10-22 15:29:32 +0200

    Rennen

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,11 @@ public class Main {
 		Rennschnecke s1 = new Rennschnecke("Hans", "Glitscho", 3);
 		Rennschnecke s2 = new Rennschnecke("Franz", "Rassanto", 5);
 		
+		Rennen r1 = new Rennen("Bilster Berg", 12);
+		
+		r1.addRennschnecke(s1);
+		r1.addRennschnecke(s2);
+		
 		s1.krieche();
 		System.out.println(s1);
 		s1.krieche();
--- src/Rennen.java
+++ src/Rennen.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
 
 public class Rennen {
+	
+	String name;
+	int teilnehmerzahl;
+	float länge;
+	
+	ArrayList<Rennschnecke> teilnehmer = new ArrayList<Rennschnecke>();
+	
+	public Rennen (String name, float länge){
+		
+		this.name = name;
+		this.länge = länge;
+		
+	}
+	
+	void addRennschnecke(Rennschnecke neueSchnecke){
+		teilnehmer.add(neueSchnecke);
+	}
+	
+	
+	
+	
+	
+	void removeRennschnecke(Rennschnecke schnecke){
+		for(Rennschnecke schnegge : teilnehmer);
+	}
+	
 
 }
